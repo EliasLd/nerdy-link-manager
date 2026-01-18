0e7d5d103bbc227a556b0ba841083f757a5c3a3b
EliasLd <area_causation872@simplelogin.fr>
2026-01-18 19:28:13 +0100

    Initialize svelte + tailwindcss

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl text-white font-bold underline">
+  Hello world!
+</h1>
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-gray-800);
+  }
+</style>
