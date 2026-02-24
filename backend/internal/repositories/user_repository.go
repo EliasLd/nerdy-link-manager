9f44ffd97f738fa2fec9708c451ff4baba6662bf
EliasLd <area_causation872@simplelogin.fr>
2026-02-24 10:35:27 +0100

    Implement base user repository utilites (create, find by id/email)

--- backend/internal/repositories/user_repository.go
+++ backend/internal/repositories/user_repository.go
@@ -27,6 +27,49 @@ func NewUserRepository(db *sql.DB) UserRepository {
 }
 
 // TODO: Implement each function
-func (r *sqliteUserRepository) Create(ctx context.Context, email, passwordHash string) error
-func (r *sqliteUserRepository) FindByEmail(ctx context.Context, email string) (*User, error)
-func (r *sqliteUserRepository) FindByID(ctx context.Context, id int64) (*User, error)
+func (r *sqliteUserRepository) Create(ctx context.Context, email, passwordHash string) error {
+	query := `
+		INSERT INTO users(email, password_hash)
+		VALUES (?, ?)
+	`
+
+	_, err := r.db.ExecContext(ctx, query, email, passwordHash)
+	return err
+
+}
+
+func (r *sqliteUserRepository) FindByEmail(ctx context.Context, email string) (*User, error) {
+	query := `
+		SELECT id, email, password_hash, created_at
+		FROM users
+		WHERE email = ?
+	`
+
+	row := r.db.QueryRowContext(ctx, query, email)
+
+	var user User
+	err := row.Scan(&user.ID, &user.Email, &user.PasswordHash, &user.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *sqliteUserRepository) FindByID(ctx context.Context, id int64) (*User, error) {
+	query := `
+		SELECT id, email, password_hash, created_at
+		FROM users
+		WHERE id = ?
+	`
+
+	row := r.db.QueryRowContext(ctx, query, id)
+
+	var user User
+	err := row.Scan(&user.ID, &user.Email, &user.PasswordHash, &user.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
