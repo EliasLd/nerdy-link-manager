a9643d7ee73fe8e6d9f953eacda83c562c520dae
EliasLd <area_causation872@simplelogin.fr>
2026-04-02 17:44:41 +0200

    Update page and style to display auth/register page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import { env } from "$env/dynamic/public";
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import { isAuthenticated } from '$lib/auth';
 
-    const PUBLIC_API_URL = env.PUBLIC_API_URL;
-    let healthStatus: string = "Loadind...";
-
-    onMount(async () => {
-        try {
-            const res = await fetch(`${PUBLIC_API_URL}/health`);
-            healthStatus = await res.text();
-        } catch (err) {
-            healthStatus = "Server unreachable";
-        }
-    });
+	onMount(() => {
+		goto(isAuthenticated() ? '/dashboard' : '/auth');
+	});
 </script>
 
-<main class="min-h-screen flex items-center justify-center bg-gray-950 text-gray-100">
-  <div class="text-center space-y-4">
-    <h1 class="text-4xl font-bold">Nerdy Link Manager</h1>
-    <p class="text-lg text-gray-400">Backend status:</p>
-    <p class="text-2xl font-mono">{healthStatus}</p>
-  </div>
-</main>
+<p class="text-gray-400 p-6">Redirecting...</p>
